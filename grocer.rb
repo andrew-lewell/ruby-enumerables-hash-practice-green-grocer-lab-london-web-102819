f6b3b8c61e7e151ddcb51e4c59253aee0099697a
Andrew Lewell <andrew.lewell@gmail.com>
2019-10-17 11:06:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,4 +42,10 @@ def checkout(cart, coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_clearance = apply_clearance(applied_coupons)
+  
+  total = 0
+  applied_clearance.each do |item, stats|
+    total = total + stats[:price]
+  end
+  total
 end
